ffbe28179c5c9ae331b55789b16aa37652aa7757
nkchambers <nkchames@gmail.com>
2023-11-16 16:55:24 -0800

    adding if_else_statements for 11-16-23

---
+++ if_else_statements.js
@@ -0,0 +1,19 @@
+// CODECADEMY JAVASCRIPT PRACTICE - if_else_statements assignment
+
+// STARTING CODEBASE
+
+let sale = true;
+
+sale = false;
+
+if (sale) {
+    console.log('Time to buy!');
+} else {
+    console.log("Time to wait for a sale.");
+}
+
+
+// Task 1 - Add an else statement to the existing if statement. 
+// Inside the code block of the else statement, console.log() the string 'Time to wait for a sale.'
+
+
